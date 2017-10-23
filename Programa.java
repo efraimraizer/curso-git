78edc7c3e5bf777478933dd3a6db1561a5f92514
Efraim Raizer <efraimraizer@hotmail.com>
2017-10-23 15:37:52 -0300

    Teste de commit do curso de git na ucsal

---
+++ Programa.java
@@ -0,0 +1,16 @@
+public class TesteGit {
+
+  public static void main(String[] args) {
+
+    List<String> lista = new ArrayList<string>();
+
+    String[] s = "TESTE";
+
+    System.out.println("TESTE");
+
+    for(lista:list){
+      
+    }
+
+  }
+}
